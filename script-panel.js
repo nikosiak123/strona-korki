3e61a8d4dc7acf3baf3ed84d851cd3a8013abc6d
nikosiak <nikodnaj@gmail.com>
2025-12-30 22:36:40 +0100

    ip

--- script-panel.js
+++ script-panel.js
@@ -6,7 +6,7 @@ const availableLevels = [
     { label: "Technikum - poziom rozszerzony", value: "liceum_rozszerzenie" }
 ];
 
-const API_BASE_URL = 'http://127.0.0.1:8080'; // Lokalny serwer deweloperski
+const API_BASE_URL = 'http://10.186.0.2:8080'; // Adres serwera na VM
 
 const daysOfWeek = ["Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela"]; // Zmieniona lista na pełne nazwy
 const daysOfWeekAPI = ["Poniedzialek", "Wtorek", "Sroda", "Czwartek", "Piatek", "Sobota", "Niedziela"]; // Nazwy używane przez API (bez polskich znaków)
