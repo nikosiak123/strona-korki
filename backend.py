57b99a0518c753107c7c3af0eb7eefd9bfa81e3a
nikosiak <nikodnaj@gmail.com>
2026-02-22 12:21:57 +0100

    nn

--- backend.py
+++ backend.py
@@ -97,7 +97,7 @@ import database # Import modułu, aby sprawdzić jego ścieżkę
 database.DB_PATH = DB_PATH
 
 print("--- Uruchamianie backend.py ---")
-logging.info(f"--- Backend łączy się z bazą danych: {DB_PATH} ---")
+logging.info(f"--- Backend łączy się z bazą danych: {os.path.abspath(DB_PATH)} ---")
 
 # Jawne wywołanie migracji bazy danych na starcie
 print("--- Inicjalizacja bazy danych ---")
